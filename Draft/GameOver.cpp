ae7a9d4f6b6a5dd2691a06a612fc7e11e4ce331b
Ilya Aleichik <Ilya.alejchik@outlook.com>
2021-08-05 22:43:45 +0300

    adding files

---
+++ Draft/GameOver.cpp
@@ -0,0 +1,14 @@
+//
+// Created by aicgt on 5.08.21.
+//
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    cout << "Game Over!\n";
+    getchar();
+    return 0;
+}
